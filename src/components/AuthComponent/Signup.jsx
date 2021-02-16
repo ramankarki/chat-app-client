6146884b187e364489a045d0cf260d94fc08da66
ramankarki <ramankarki40@gmail.com>
2021-02-16 21:31:51 +0545

    wired newUserData props from redux store and set action creator to make controlled fields

--- src/components/AuthComponent/Signup.jsx
+++ src/components/AuthComponent/Signup.jsx
@@ -1,35 +1,174 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { login } from "../../utils/Routes";
+import { connect } from "react-redux";
 
-function Signup() {
-  const onSubmit = (event) => {
+import { login } from "../../utils/Routes";
+import { setNewUserData } from "../../actions";
+import { newUserDataState } from "../../actions/types";
+
+class Signup extends React.Component {
+  onSubmit = (event) => {
     event.preventDefault();
+
+    if (this.props.newUserData.password.length >= 12) {
+      if (
+        this.props.newUserData.password ===
+        this.props.newUserData.confirmpassword
+      ) {
+        this.props.setNewUserData(
+          true,
+          this.props.newUserData.username,
+          this.props.newUserData.email,
+          this.props.newUserData.password,
+          this.props.newUserData.confirmpassword,
+          newUserDataState.submitting
+        );
+      } else {
+        this.props.setNewUserData(
+          true,
+          this.props.newUserData.username,
+          this.props.newUserData.email,
+          this.props.newUserData.password,
+          this.props.newUserData.confirmpassword,
+          newUserDataState.unmatchedValidation
+        );
+      }
+    } else {
+      this.props.setNewUserData(
+        true,
+        this.props.newUserData.username,
+        this.props.newUserData.email,
+        this.props.newUserData.password,
+        this.props.newUserData.confirmpassword,
+        newUserDataState.lengthValidation
+      );
+    }
   };
 
-  return (
-    <form className="form" onSubmit={onSubmit}>
-      <h2 className="form__heading">Sign up</h2>
+  onSignupFieldChange = (event, field) => {
+    switch (field) {
+      case "username":
+        this.props.setNewUserData(
+          true,
+          event.target.value,
+          this.props.newUserData.email,
+          this.props.newUserData.password,
+          this.props.newUserData.confirmpassword,
+          newUserDataState.typing
+        );
+        break;
+      case "email":
+        this.props.setNewUserData(
+          true,
+          this.props.newUserData.username,
+          event.target.value,
+          this.props.newUserData.password,
+          this.props.newUserData.confirmpassword,
+          newUserDataState.typing
+        );
+        break;
+      case "password":
+        this.props.setNewUserData(
+          true,
+          this.props.newUserData.username,
+          this.props.newUserData.email,
+          event.target.value,
+          this.props.newUserData.confirmpassword,
+          newUserDataState.typing
+        );
+        break;
+      case "confirmpassword":
+        this.props.setNewUserData(
+          true,
+          this.props.newUserData.username,
+          this.props.newUserData.email,
+          this.props.newUserData.password,
+          event.target.value,
+          newUserDataState.typing
+        );
+        break;
+      default:
+        this.props.setNewUserData(
+          true,
+          this.props.newUserData.username,
+          this.props.newUserData.email,
+          this.props.newUserData.password,
+          this.props.newUserData.confirmpassword,
+          newUserDataState.typing
+        );
+    }
+  };
 
-      <div className="form__fieldsWrapper">
-        <input type="text" placeholder="Username" />
-        <input type="email" placeholder="Email" />
-        <input type="text" placeholder="Password" />
-        <input type="text" placeholder="Confirm password" />
-      </div>
+  componentDidMount() {
+    this.props.setNewUserData(
+      true,
+      "",
+      "",
+      "",
+      "",
+      newUserDataState.emptyFields
+    );
+  }
 
-      <button type="submit" className="form__submitBtn">
-        Signup
-      </button>
+  render() {
+    if (this.props.newUserData) {
+      return (
+        <form className="form" onSubmit={this.onSubmit}>
+          <h2 className="form__heading">Sign up</h2>
 
-      <p className="form__switchAuth">
-        Already have an account ?{" "}
-        <Link to={login} className="form__log-or-sign">
-          Login
-        </Link>{" "}
-      </p>
-    </form>
-  );
+          <div className="form__fieldsWrapper">
+            <input
+              type="text"
+              placeholder="Username"
+              value={this.props.newUserData.username}
+              onChange={(event) => this.onSignupFieldChange(event, "username")}
+              required
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              value={this.props.newUserData.email}
+              onChange={(event) => this.onSignupFieldChange(event, "email")}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Password"
+              value={this.props.newUserData.password}
+              onChange={(event) => this.onSignupFieldChange(event, "password")}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Confirm password"
+              value={this.props.newUserData.confirmpassword}
+              onChange={(event) =>
+                this.onSignupFieldChange(event, "confirmpassword")
+              }
+              required
+            />
+          </div>
+
+          <button type="submit" className="form__submitBtn">
+            Signup
+          </button>
+
+          <p className="form__switchAuth">
+            Already have an account ?{" "}
+            <Link to={login} className="form__log-or-sign">
+              Login
+            </Link>{" "}
+          </p>
+        </form>
+      );
+    } else {
+      return "";
+    }
+  }
 }
 
-export default Signup;
+const mapStateToProps = ({ newUserData }) => {
+  return { newUserData };
+};
+
+export default connect(mapStateToProps, { setNewUserData })(Signup);
