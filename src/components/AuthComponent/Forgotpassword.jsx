dc63f0b8aca5311524e602ec7545b4c70140ac53
ramankarki <ramankarki40@gmail.com>
2021-02-16 12:14:35 +0545

    added forgot password desc

--- src/components/AuthComponent/Forgotpassword.jsx
+++ src/components/AuthComponent/Forgotpassword.jsx
@@ -9,6 +9,10 @@ function Signup() {
     <form className="form" onSubmit={onSubmit}>
       <h2 className="form__heading">Forgot Password</h2>
 
+      <p className="form__forgotPasswordDesc">
+        Get reset token to your mailbox
+      </p>
+
       <div className="form__fieldsWrapper">
         <input type="email" placeholder="Email" />
       </div>
