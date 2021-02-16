f5f6300796ecdb4c2e0b7641436475ee20a88857
ramankarki <ramankarki40@gmail.com>
2021-02-16 10:17:24 +0545

    imported signup login and homepage component and wired with hashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import AuthComponent from "./AuthComponent/AuthComponent";
+import Signup from "./AuthComponent/Signup";
+import Login from "./AuthComponent/Login";
+import Homepage from "./Homepage/Homepage";
 import "./App.scss";
 
 class App extends React.Component {
   render() {
-    return <div>hello world</div>;
+    return (
+      <div>
+        <HashRouter>
+          <Route path="/" exact component={Homepage} />
+          <Route path="/login" exact>
+            <AuthComponent>
+              <Login />
+            </AuthComponent>
+          </Route>
+          <Route path="/signup" exact>
+            <AuthComponent>
+              <Signup />
+            </AuthComponent>
+          </Route>
+        </HashRouter>
+      </div>
+    );
   }
 }
 
