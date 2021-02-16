23a2f567ac0533cc7127675fb9e3c39d525604a6
ramankarki <ramankarki40@gmail.com>
2021-02-16 19:08:27 +0545

    wired redux and redux thunk with extension for dev

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
 
+import reducers from "./reducers";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhanchers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store;
+
+if (process.env.REACT === "development") {
+  store = createStore(reducers, composeEnhanchers(applyMiddleware(reduxThunk)));
+} else {
+  store = createStore(reducers, applyMiddleware(reduxThunk));
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
