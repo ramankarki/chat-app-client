f7061b7c1e532828a560767da012c75589c45d87
ramankarki <ramankarki40@gmail.com>
2021-02-17 13:49:37 +0545

    action creator to signup user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-import { NEW_USER_DATA } from "./types";
+import { NEW_USER_DATA, newUserDataState } from "./types";
+import axios from "../utils/axios";
+import history from "../utils/history";
+import { emailConfirmation } from "../utils/Routes";
 
 export const setNewUserData = (
   newUser,
@@ -24,5 +27,29 @@ export const setNewUserData = (
       type: NEW_USER_DATA,
       payload: null,
     };
+  }
+};
+
+export const signupUser = () => async (dispatch, getState) => {
+  const { newUserData } = getState();
+
+  try {
+    await axios.post("/api/v1/users/signup", newUserData);
+    dispatch({
+      type: NEW_USER_DATA,
+      payload: {
+        ...newUserData,
+        state: newUserDataState.submitted,
+      },
+    });
+    history.push(emailConfirmation);
+  } catch (err) {
+    dispatch({
+      type: NEW_USER_DATA,
+      payload: {
+        ...newUserData,
+        state: newUserDataState.failed,
+      },
+    });
   }
 };
