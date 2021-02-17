8d22d407be86f084ae13840e64968ddf1f38e545
ramankarki <ramankarki40@gmail.com>
2021-02-17 19:01:16 +0545

    action creator to resend activation email and to activate account

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import { NEW_USER_DATA, newUserDataState } from "./types";
 import axios from "../utils/axios";
 import history from "../utils/history";
-import { emailConfirmation } from "../utils/Routes";
+import { emailConfirmation, conversations } from "../utils/Routes";
 
 export const setNewUserData = (
   newUser,
@@ -49,6 +49,78 @@ export const signupUser = () => async (dispatch, getState) => {
       payload: {
         ...newUserData,
         state: newUserDataState.failed,
+      },
+    });
+  }
+};
+
+export const resendAccountActivateEmail = () => async (dispatch, getState) => {
+  const { newUserData } = getState();
+
+  dispatch({
+    type: NEW_USER_DATA,
+    payload: {
+      ...newUserData,
+      state: newUserDataState.resubmitting,
+    },
+  });
+
+  try {
+    await axios.post("/api/v1/users/resendActivationLink", {
+      email: newUserData.email,
+    });
+    dispatch({
+      type: NEW_USER_DATA,
+      payload: {
+        ...newUserData,
+        state: newUserDataState.submitted,
+      },
+    });
+    history.push(emailConfirmation);
+  } catch (err) {
+    dispatch({
+      type: NEW_USER_DATA,
+      payload: {
+        ...newUserData,
+        state: newUserDataState.emailNotExist,
+      },
+    });
+  }
+};
+
+export const activateAccount = (token) => async (dispatch) => {
+  dispatch({
+    type: NEW_USER_DATA,
+    payload: {
+      username: "",
+      email: "",
+      password: "",
+      confirmpassword: "",
+      state: newUserDataState.activatingAccount,
+    },
+  });
+  try {
+    await axios.get(`/api/v1/users/activateAccount/${token}`);
+    dispatch({
+      type: NEW_USER_DATA,
+      payload: {
+        username: "",
+        email: "",
+        password: "",
+        confirmpassword: "",
+        state: newUserDataState.accountActivated,
+      },
+    });
+    history.push(conversations);
+  } catch (err) {
+    dispatch({
+      type: NEW_USER_DATA,
+      payload: {
+        username: "",
+        email: "",
+        password: "",
+        confirmpassword: "",
+        state: newUserDataState.invalidToken,
       },
     });
   }
