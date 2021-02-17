f540d2262e393f5acefba76fa7ebaa1f10d369f8
ramankarki <ramankarki40@gmail.com>
2021-02-17 22:48:28 +0545

    added Alerts for success and failed message, used spinner for loading and action creator to post email to server

--- src/components/AuthComponent/Forgotpassword.jsx
+++ src/components/AuthComponent/Forgotpassword.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { setForgotPassword } from "../../actions";
+import { setForgotPassword, sendForgotPassword } from "../../actions";
+import Alert from "react-bootstrap/Alert";
+import spinner from "../spinner.svg";
 
 class ForgotPassword extends React.Component {
   onSubmit = (event) => {
     event.preventDefault();
+
+    if (this.props.forgotPassword.state === "submitting") return;
+
+    this.props.sendForgotPassword();
   };
 
   componentDidMount() {
@@ -19,8 +25,26 @@ class ForgotPassword extends React.Component {
   render() {
     if (!this.props.forgotPassword) return "";
 
+    const show =
+      this.props.forgotPassword.state === "submitted" ||
+      this.props.forgotPassword.state === "failed";
+
+    const variant =
+      this.props.forgotPassword.state === "submitted" ? "success" : "danger";
+
+    const failedMessage =
+      variant === "success"
+        ? "An email has been sent to your mail account. It will only valid for 10min"
+        : "Either there is no user with this email or has not activated account";
+
     return (
       <form className="form" onSubmit={this.onSubmit}>
+        <div className="signup-warning">
+          <Alert show={show} variant={variant}>
+            <p>{failedMessage}</p>
+          </Alert>
+        </div>
+
         <h2 className="form__heading">Forgot Password</h2>
 
         <p className="form__forgotPasswordDesc">
@@ -32,6 +56,7 @@ class ForgotPassword extends React.Component {
             type="email"
             placeholder="Email"
             value={this.props.forgotPassword.email}
+            required
             onChange={(event) =>
               this.props.setForgotPassword(event.target.value, "typing", true)
             }
@@ -39,7 +64,15 @@ class ForgotPassword extends React.Component {
         </div>
 
         <button type="submit" className="form__submitBtn">
-          Send
+          {this.props.forgotPassword.state === "submitting" ? (
+            <img
+              className="icon-spinner"
+              src={spinner}
+              alt="spinner icon svg"
+            />
+          ) : (
+            "Send"
+          )}
         </button>
       </form>
     );
@@ -50,4 +83,7 @@ const mapStateToProps = ({ forgotPassword }) => {
   return { forgotPassword };
 };
 
-export default connect(mapStateToProps, { setForgotPassword })(ForgotPassword);
+export default connect(mapStateToProps, {
+  setForgotPassword,
+  sendForgotPassword,
+})(ForgotPassword);
