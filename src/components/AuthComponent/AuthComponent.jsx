f3aa5ee3f27a6a4516530f9cdb9c4f21c5e6eaa8
ramankarki <ramankarki40@gmail.com>
2021-02-17 11:20:51 +0545

    added app logo and position fixed for mobile

--- src/components/AuthComponent/AuthComponent.jsx
+++ src/components/AuthComponent/AuthComponent.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import logo from "../logo.png";
 import "./AuthComponent.scss";
 
 class AuthComponent extends React.Component {
   render() {
     return (
       <div className="auth-component">
-        <div className="app-logo-aside"></div>
+        <div className="app-logo-aside">
+          <img src={logo} alt="chat app logo" />
+        </div>
         <div className="form-wrapper">{this.props.children}</div>
       </div>
     );
