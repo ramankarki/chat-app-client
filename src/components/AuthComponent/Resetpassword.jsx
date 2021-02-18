65356ed073a8689d9fa18b703045a3f38cd0a0f0
ramankarki <ramankarki40@gmail.com>
2021-02-18 13:32:11 +0545

    wired resetPassword action creator, error Alert component and loading spinner

--- src/components/AuthComponent/Resetpassword.jsx
+++ src/components/AuthComponent/Resetpassword.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
-import { setResetPassword } from "../../actions";
+import { setResetPassword, resetPassword } from "../../actions";
 import { newUserDataState } from "../../actions/types";
+import Alert from "react-bootstrap/Alert";
+import spinner from "../spinner.svg";
 
 class Resetpassword extends React.Component {
   onSubmit = (event) => {
     event.preventDefault();
+
+    if (this.props.resetPasswordData.state === "submitting") return;
 
     const { resetPasswordData } = this.props;
 
@@ -27,7 +31,7 @@ class Resetpassword extends React.Component {
         newUserDataState.unmatchedValidation
       );
 
-    console.log("submitting");
+    this.props.resetPassword(this.props.match.params.token);
   };
 
   onInputFieldChange = (event, field) => {
@@ -64,8 +68,29 @@ class Resetpassword extends React.Component {
 
   render() {
     if (!this.props.resetPasswordData) return "";
+
+    const validation = this.props.resetPasswordData.state.startsWith(
+      "validation"
+    );
+    const invalidToken =
+      this.props.resetPasswordData.state === newUserDataState.invalidToken;
+
+    let failedMessage;
+    if (validation) {
+      failedMessage = this.props.resetPasswordData.state.split(",")[1];
+    } else if (invalidToken) {
+      failedMessage =
+        "Something went wrong ! Maybe token is invalid or has expired.";
+    }
+
     return (
       <form className="form" onSubmit={this.onSubmit}>
+        <div className="signup-warning">
+          <Alert show={validation || invalidToken} variant="danger">
+            <p>{failedMessage}</p>
+          </Alert>
+        </div>
+
         <h2 className="form__heading">Reset Password</h2>
 
         <div className="form__fieldsWrapper">
@@ -88,7 +113,16 @@ class Resetpassword extends React.Component {
         </div>
 
         <button type="submit" className="form__submitBtn">
-          Update
+          {this.props.resetPasswordData.state ===
+          newUserDataState.submitting ? (
+            <img
+              className="icon-spinner"
+              src={spinner}
+              alt="spinner icon svg"
+            />
+          ) : (
+            "Update"
+          )}
         </button>
       </form>
     );
@@ -100,5 +134,5 @@ const mapStateToProps = ({ resetPasswordData }) => {
 };
 
 export default withRouter(
-  connect(mapStateToProps, { setResetPassword })(Resetpassword)
+  connect(mapStateToProps, { setResetPassword, resetPassword })(Resetpassword)
 );
