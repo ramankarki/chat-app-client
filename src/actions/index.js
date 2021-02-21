a7be9cd7bc319e5bada73e90d1c1a23cc0d0e2eb
ramankarki <ramankarki40@gmail.com>
2021-02-21 19:52:19 +0545

    action creator for resetPassword, getMe, getConversations, getUsers, setActiveConversation, createConversation, setMessageData, sendMessage, userInserted, userUpdated, userDeleted, conversationInserted, conversationDeleted, messageInserted, updateOnlineState all at once after not commiting for lone time

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,10 +5,14 @@ import {
   LOGIN_USER_DATA,
   FORGOT_PASSWORD,
   RESET_PASSWORD_DATA,
+  CONVERSATIONS,
+  USERS,
+  ACTIVE_CONVERSATION,
+  MESSAGE_DATA,
 } from "./types";
 import axios from "../utils/axios";
 import history from "../utils/history";
-import { emailConfirmation, conversations } from "../utils/Routes";
+import { emailConfirmation, conversations, login } from "../utils/Routes";
 
 export const setNewUserData = (
   newUser,
@@ -39,6 +43,7 @@ export const setNewUserData = (
 
 export const signupUser = () => async (dispatch, getState) => {
   const { newUserData } = getState();
+  newUserData.joinedAt = Date.now();
 
   try {
     await axios.post("/api/v1/users/signup", newUserData);
@@ -108,24 +113,12 @@ export const activateAccount = (token) => async (dispatch) => {
   });
   try {
     const user = await axios.get(`/api/v1/users/activateAccount/${token}`);
-    const updateActiveState = await axios.patch(
-      "/api/v1/users/updateMe",
-      {
-        isUserActive: true,
-      },
-      {
-        headers: {
-          authorization: `Bearer ${user.data.token}`,
-        },
-      }
-    );
 
     dispatch({
       type: AUTH_USER,
       payload: {
-        isLoggedIn: true,
         token: user.data.token,
-        user: updateActiveState.data.user,
+        user: user.data.user,
       },
     });
 
@@ -184,24 +177,12 @@ export const loginUser = () => async (dispatch, getState) => {
 
   try {
     const user = await axios.post("/api/v1/users/login", loginUserData);
-    const updateActiveState = await axios.patch(
-      "/api/v1/users/updateMe",
-      {
-        isUserActive: true,
-      },
-      {
-        headers: {
-          authorization: `Bearer ${user.data.token}`,
-        },
-      }
-    );
 
     dispatch({
       type: AUTH_USER,
       payload: {
-        isLoggedIn: true,
         token: user.data.token,
-        user: updateActiveState.data.user,
+        user: user.data.user,
       },
     });
 
@@ -301,24 +282,11 @@ export const resetPassword = (token) => async (dispatch, getState) => {
       resetPasswordData
     );
 
-    const updateActiveState = await axios.patch(
-      "/api/v1/users/updateMe",
-      {
-        isUserActive: true,
-      },
-      {
-        headers: {
-          authorization: `Bearer ${user.data.token}`,
-        },
-      }
-    );
-
     dispatch({
       type: AUTH_USER,
       payload: {
-        isLoggedIn: true,
         token: user.data.token,
-        user: updateActiveState.data.user,
+        user: user.data.user,
       },
     });
 
@@ -331,5 +299,349 @@ export const resetPassword = (token) => async (dispatch, getState) => {
         state: newUserDataState.invalidToken,
       },
     });
+  }
+};
+
+export const getMe = () => async (dispatch, getState) => {
+  const { auth } = getState();
+  try {
+    const updateActiveState = await axios.patch(
+      "/api/v1/users/updateMe",
+      {
+        isUserActive: true,
+      },
+      {
+        headers: {
+          authorization: `Bearer ${auth.token}`,
+        },
+      }
+    );
+
+    dispatch({
+      type: AUTH_USER,
+      payload: {
+        ...auth,
+        user: updateActiveState.data.user,
+      },
+    });
+    history.push(conversations);
+  } catch (err) {
+    history.push(login);
+    console.log(err);
+  }
+};
+
+export const getConversations = () => async (dispatch, getState) => {
+  const { auth } = getState();
+
+  try {
+    const conversations = await axios.get("/api/v1/conversations", {
+      headers: {
+        authorization: `Bearer ${auth.token}`,
+      },
+    });
+
+    let count = 0;
+    conversations.data.conversations.forEach((con) => {
+      const lastMsg = con.messages[con.messages.length - 1];
+
+      if (!lastMsg || lastMsg.user !== auth.user._id) {
+        count++;
+      }
+    });
+
+    dispatch({
+      type: CONVERSATIONS,
+      payload: {
+        data: conversations.data.conversations,
+        notifications: count,
+      },
+    });
+  } catch (err) {
+    history.push(login);
+  }
+};
+
+export const getUsers = () => async (dispatch, getState) => {
+  const { auth } = getState();
+
+  try {
+    const users = await axios.get("/api/v1/users", {
+      headers: {
+        authorization: `Bearer ${auth.token}`,
+      },
+    });
+
+    dispatch({
+      type: USERS,
+      payload: [...users.data.users],
+    });
+  } catch (err) {
+    history.push(login);
+  }
+};
+
+export const setActiveConversation = (conversation) => {
+  return {
+    type: ACTIVE_CONVERSATION,
+    payload: conversation,
+  };
+};
+
+export const createConversation = (id) => async (dispatch, getState) => {
+  const { auth } = getState();
+
+  dispatch({
+    type: ACTIVE_CONVERSATION,
+    payload: {
+      state: "creating",
+      id,
+    },
+  });
+
+  try {
+    await axios.post(
+      "/api/v1/conversations",
+      { user1: auth.user._id, user2: id, createdAt: Date.now() },
+      {
+        headers: {
+          authorization: `Bearer ${auth.token}`,
+        },
+      }
+    );
+  } catch (err) {
+    dispatch({
+      type: ACTIVE_CONVERSATION,
+      payload: {
+        state: "failed",
+      },
+    });
+  }
+};
+
+export const setMessageData = (message, state) => {
+  return {
+    type: MESSAGE_DATA,
+    payload: {
+      message,
+      state,
+    },
+  };
+};
+
+export const sendMessage = () => async (dispatch, getState) => {
+  const { message, auth, activeConversation } = getState();
+
+  dispatch({
+    type: MESSAGE_DATA,
+    payload: {
+      message: message.message,
+      state: "submitting",
+    },
+  });
+
+  try {
+    await axios.post(
+      "/api/v1/messages",
+      {
+        conversation: activeConversation._id,
+        user: auth.user._id,
+        message: message.message,
+        createdAt: Date.now(),
+      },
+      {
+        headers: {
+          authorization: `Bearer ${auth.token}`,
+        },
+      }
+    );
+
+    dispatch({
+      type: MESSAGE_DATA,
+      payload: {
+        message: "",
+        state: "submitted",
+      },
+    });
+  } catch (err) {
+    dispatch({
+      type: MESSAGE_DATA,
+      payload: {
+        message: message.message,
+        state: "failed",
+      },
+    });
+  }
+};
+
+export const userInserted = (user) => (dispatch, getState) => {
+  const { users } = getState();
+  if (users.findIndex((sUser) => sUser._id === user._id) === -1) {
+    const newUsers = [...users, user];
+
+    dispatch({
+      type: USERS,
+      payload: [...newUsers],
+    });
+  }
+};
+
+export const userUpdated = (updates) => (dispatch, getState) => {
+  let { users } = getState();
+
+  let updatedUser;
+  let i;
+  for (i = 0; i < users.length; i++) {
+    if (users[i]._id === updates.id) {
+      updatedUser = {
+        ...users[i],
+        ...updates.updatedFields,
+      };
+      break;
+    }
+  }
+
+  users = [...users.slice(0, i), updatedUser, ...users.slice(i + 1)];
+
+  dispatch({
+    type: USERS,
+    payload: users,
+  });
+};
+
+export const userDeleted = (id) => (dispatch, getState) => {
+  const { users } = getState();
+
+  const newUsers = users.filter((user) => user._id !== id);
+
+  dispatch({
+    type: USERS,
+    payload: [...newUsers],
+  });
+};
+
+export const conversationInserted = (doc) => (dispatch, getState) => {
+  let { conversations, auth, activeConversation } = getState();
+
+  // insert conversation only if it is for auth user
+  if (doc.user1 === auth.user._id || doc.user2 === auth.user._id) {
+    if (conversations.data.findIndex((con) => con._id === doc._id) === -1) {
+      conversations.data = [...conversations.data, doc];
+
+      let count = 0;
+      conversations.data.forEach((con) => {
+        const lastMsg = con.messages[con.messages.length - 1];
+
+        if (!lastMsg || lastMsg.user !== auth.user._id) {
+          count++;
+        }
+      });
+
+      dispatch({
+        type: CONVERSATIONS,
+        payload: {
+          data: [...conversations.data],
+          notifications: count,
+        },
+      });
+
+      if (activeConversation && activeConversation.id) {
+        dispatch({
+          type: ACTIVE_CONVERSATION,
+          payload: doc,
+        });
+      }
+    }
+  }
+};
+
+export const conversationDeleted = (id) => (dispatch, getState) => {
+  let { conversations, auth, activeConversation } = getState();
+
+  if (activeConversation._id === id) {
+    dispatch({
+      type: ACTIVE_CONVERSATION,
+      payload: null,
+    });
+  }
+
+  conversations.data = conversations.data.filter((con) => con._id !== id);
+
+  let count = 0;
+  conversations.data.forEach((con) => {
+    const lastMsg = con.messages[con.messages.length - 1];
+
+    if (!lastMsg || lastMsg.user !== auth.user._id) {
+      count++;
+    }
+  });
+
+  dispatch({
+    type: CONVERSATIONS,
+    payload: {
+      data: [...conversations.data],
+      notifications: count,
+    },
+  });
+};
+
+export const messageInserted = (doc) => (dispatch, getState) => {
+  const { conversations, auth } = getState();
+
+  let i = conversations.data.findIndex((con) => con._id === doc.conversation);
+
+  if (i >= 0) {
+    if (
+      conversations.data[i].messages.findIndex((el) => el._id === doc._id) ===
+      -1
+    ) {
+      conversations.data[i].messages = [...conversations.data[i].messages, doc];
+
+      const newConversations = [...conversations.data];
+
+      let count = 0;
+      newConversations.forEach((con) => {
+        const lastMsg = con.messages[con.messages.length - 1];
+
+        if (!lastMsg || lastMsg.user !== auth.user._id) {
+          count++;
+        }
+      });
+
+      dispatch({
+        type: CONVERSATIONS,
+        payload: {
+          data: newConversations,
+          notifications: count,
+        },
+      });
+
+      if (doc.user === auth.user._id) {
+        dispatch({
+          type: ACTIVE_CONVERSATION,
+          payload: newConversations[i],
+        });
+      }
+    }
+  }
+};
+
+export const updateOnlineState = (state) => async (dispatch, getState) => {
+  const { auth } = getState();
+
+  try {
+    await axios.patch(
+      "/api/v1/users/updateMe",
+      {
+        isUserActive: state,
+      },
+      {
+        headers: {
+          authorization: `Bearer ${auth.token}`,
+        },
+      }
+    );
+  } catch (err) {
+    history.push(login);
   }
 };
