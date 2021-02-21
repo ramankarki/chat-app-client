fa69dde9d72a201f43f0042cd9515a35cd832b39
ramankarki <ramankarki40@gmail.com>
2021-02-21 23:45:52 +0545

    responsive homepage complete

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { login, signup } from "../../utils/Routes";
+import illustration from "./illustration.svg";
+import logo from "../logo.png";
 import "./Homepage.scss";
 
 class Homepage extends React.Component {
   render() {
     return (
-      <div>
-        this is homepage
-        <div>
-          <Link to="/login">login</Link>
+      <div className="container landing">
+        <div className="landingPage-wrapper">
+          <div className="landingPage">
+            <div className="landingPage-actionWrapper">
+              <div className="landingPage-action">
+                <h6 className="landingPage-heading">
+                  Search for your friends and
+                  <span> Start Chatting</span>
+                </h6>
+                <div className="landingPage-buttons">
+                  <Link to={signup} className="signup">
+                    signup
+                  </Link>
+                  <Link to={login} className="login">
+                    login
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <picture className="landingPage-image">
+              <img src={illustration} alt="mobile chatting illustration" />
+            </picture>
+          </div>
         </div>
-        <div>
-          <Link to="/signup">signup</Link>
-        </div>
+
+        <img className="landing-app-logo" src={logo} alt="app logo" />
+        <p className="github-link">
+          Chat App project by{" "}
+          <a href="https://github.com/ramankarki">Raman Karki</a>
+        </p>
       </div>
     );
   }
