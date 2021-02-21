7c0e723d31a348896e6dfc88422f9bd8047d1496
ramankarki <ramankarki40@gmail.com>
2021-02-21 19:43:49 +0545

    secret keys for pusher

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+if (process.env.REACT_APP_CHAT_APP === "development") {
+  module.exports = require("./dev");
+} else {
+  module.exports = {
+    PUSHER_KEY: process.env.PUSHER_KEY,
+    PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
+  };
+}
