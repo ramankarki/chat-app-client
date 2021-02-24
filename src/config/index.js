3599a11b80de9daddb19ffe06cdd73b07b8e987e
ramankarki <ramankarki40@gmail.com>
2021-02-24 21:51:57 +0545

    commented conditional importing and returned object directly

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,13 @@
-if (process.env.REACT_APP_CHAT_APP === "development") {
-  module.exports = require("./dev");
-} else {
-  module.exports = {
-    PUSHER_KEY: process.env.PUSHER_KEY,
-    PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
-  };
-}
+module.exports = {
+  PUSHER_KEY: process.env.PUSHER_KEY,
+  PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
+};
+
+// if (process.env.REACT_APP_CHAT_APP === "development") {
+//   module.exports = require("./dev");
+// } else {
+//   module.exports = {
+//     PUSHER_KEY: process.env.PUSHER_KEY,
+//     PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
+//   };
+// }
