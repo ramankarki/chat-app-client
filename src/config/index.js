7377da13755e372492b26b020008afba48a9190d
ramankarki <ramankarki40@gmail.com>
2021-02-25 22:05:09 +0545

    back to normal

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,8 @@
-module.exports = {
-  PUSHER_KEY: process.env.REACT_APP_PUSHER_KEY,
-  PUSHER_CLUSTER: process.env.REACT_APP_PUSHER_CLUSTER,
-};
-
-// if (process.env.REACT_APP_CHAT_APP === "development") {
-//   module.exports = require("./dev");
-// } else {
-//   module.exports = {
-//     PUSHER_KEY: process.env.PUSHER_KEY,
-//     PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
-//   };
-// }
+if (process.env.REACT_APP_CHAT_APP === "development") {
+  module.exports = require("./dev");
+} else {
+  module.exports = {
+    PUSHER_KEY: process.env.REACT_APP_PUSHER_KEY,
+    PUSHER_CLUSTER: process.env.REACT_APP_PUSHER_CLUSTER,
+  };
+}
